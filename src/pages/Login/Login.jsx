bcc04d3cabe90e9181991dae2c043474b4757551
rtaukir26 <tauqueer.ansari@bluebinaries.com>
2025-01-23 12:03:16 +0530

    login done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -7,6 +7,7 @@ const Login = () => {
     <div className="login-main">
       <div className="login-body">
         <div className="login-wrap">
+          {/* login container */}
           <div className="login-container">
             <div className="wrapper">
               <span className="mb-4">RPP</span>
@@ -30,7 +31,33 @@ const Login = () => {
               </div>
             </div>
           </div>
-          <div className="shop-info-container">shop content</div>
+
+          {/* comp content */}
+
+          <div className="shop-info-container">
+            <div className="wrapper">
+              <div className="top-con">
+                <p className="fs-4 my-4">What's your thoughts</p>
+                <p className="mt-5">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Laborum provident dicta fugit eveniet ipsam sit cupiditate
+                  quidem illum! Totam, distinctio.
+                </p>
+                <div>clients</div>
+              </div>
+
+              <div className="bottom-con">
+                <strong className="">
+                  Get your best parts here and best quality in best price
+                </strong>
+                <p className="mt-4">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Iusto, corrupti veniam quibusdam excepturi repellat error
+                  explicabo ipsam tempore repudiandae nobis!
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
